f9aebcb8e32f2bd97d7763f490f3f3504f6d6411
Jan Eglinger <jan.eglinger@fmi.ch>
2026-05-13 11:33:49 +0200

    Add consolidate_tables_task
    Also update manifest by running:
      pixi run create-manifest

--- src/fractal_hcs_qc/dev/task_list.py
+++ src/fractal_hcs_qc/dev/task_list.py
@@ -1,32 +1,27 @@
 """Contains the list of tasks available to fractal."""
 
 from fractal_task_tools.task_models import (
-    ParallelTask,
+    NonParallelTask,
 )
 
-AUTHORS = "Jan Eglinger"
-
+AUTHORS = "Facility for Advanced Imaging and Microscopy (FAIM), FMI, Basel"
 
 DOCS_LINK = None
 
 
 TASK_LIST = [
-    
-    
-    ParallelTask(
-        name="Gaussian Blur",
-        executable="gaussian_blur_task.py",
+    NonParallelTask(
+        name="Consolidate Tables",
+        executable="consolidate_tables_task.py",
         # Modify the meta according to your task requirements
         # If the task requires a GPU, add "needs_gpu": True
         meta={"cpus_per_task": 1, "mem": 4000},
-        category="Image Processing",
-        tags=["Denoising", "Gaussian Blur"],
-        docs_info="file:docs_info/gaussian_blur_task.md",
+        category="Table Processing",
+        tags=["Tables", "HCS"],
+        docs_info="file:docs_info/consolidate_tables_task.md",
         # Uncomment the following line to forbid re-running the task on already
         # processed images
         # input_types={"is_blurred": False},
-        output_types={"is_blurred": True},
+        # output_types={"is_blurred": True},
     ),
-    
-    
 ]
